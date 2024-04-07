db80876b92fcdb702b7da3f921006ada0b1b7ff4
KarinaShtankova <shtankovads@gmail.com>
2024-04-08 02:45:43 +0300

    bugfix add lazy import

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { Suspense, useEffect } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import HomePage from './pages/HomePage/HomePage';
-import ContactsPage from './pages/ContactsPage/ContactsPage';
 import Loader from './components/Loader/Loader';
-import NotFound from './pages/NotFoundPage/NotFoundPage';
 import { useDispatch, useSelector } from 'react-redux';
 import { refreshUser } from './redux/auth/operations';
 import { selectIsRefreshing } from './redux/auth/selectors';
 import RestrictedRoute from './RestrictedRoute';
 import PrivateRoute from './PrivateRoute';
 import Layout from './components/Layout/Layout';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const RegisterPage = lazy(() => import('./pages/RegisterPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const ContactsPage = lazy(() => import('./pages/ContactsPage'));
+const NotFound = lazy(() => import('./pages/NotFoundPage'));
 
 export default function App() {
   const isRefreshing = useSelector(selectIsRefreshing);
@@ -23,28 +24,28 @@ export default function App() {
 
   return (
     <Layout>
-
       {isRefreshing ? (
-        <Loader />) : (
-          <Suspense fallback={<Loader />}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route
-                path="/contacts"
-                element={<PrivateRoute component={<ContactsPage />} />}
-              />
-              <Route
-                path="/register"
-                element={<RestrictedRoute component={<RegisterPage />} />}
-              />
-              <Route
-                path="/login"
-                element={<RestrictedRoute component={<LoginPage />} />}
-              />
+        <Loader />
+      ) : (
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/contacts"
+              element={<PrivateRoute component={<ContactsPage />} />}
+            />
+            <Route
+              path="/register"
+              element={<RestrictedRoute component={<RegisterPage />} />}
+            />
+            <Route
+              path="/login"
+              element={<RestrictedRoute component={<LoginPage />} />}
+            />
 
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Suspense>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       )}
     </Layout>
   );
